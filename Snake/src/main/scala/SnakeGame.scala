3b99ea792c767e97981b07d4b4f8c72089783488
Rodion Ofatenko <r.ofatenko@andersenlab.com>
2020-05-17 14:30:39 +0300

    Snake game add food

--- Snake/src/main/scala/SnakeGame.scala
+++ Snake/src/main/scala/SnakeGame.scala
@@ -3,8 +3,6 @@ import java.awt.{Color, Component}
 import javax.swing.JFrame
 import jelements.JSnakeBodyPart
 
-import scala.util.Random
-
 class SnakeGame {
 
   import SnakeGame.speed
@@ -12,11 +10,11 @@ class SnakeGame {
 
   def run: Unit = {
     val frame = JFrameFactory.create
-    val listenerFirst = new WASDListener(reactToKey(_, x = 20, y = 20, frame = frame, randomPoint = JSnakeBodyPart.generate(Color.RED)))
+    val listenerFirst = new WASDListener(reactToKey(_, x = 20, y = 20, snakeLength = 1, frame = frame, randomPoint = JSnakeBodyPart.generate(Color.RED)))
     frame.addKeyListener(listenerFirst)
   }
 
-  private def reactToKey(key: HandledKey, x: Int, y: Int, frame: JFrame, randomPoint: JSnakeBodyPart): Unit = {
+  private def reactToKey(key: HandledKey, x: Int, y: Int, snakeLength: Int, frame: JFrame, randomPoint: JSnakeBodyPart): Unit = {
     val (newX, newY) = key match {
       case W => (x, y - speed)
       case A => (x - speed, y)
@@ -26,30 +24,31 @@ class SnakeGame {
         throw new Exception("Game over")
     }
 
-    val components = frame.getContentPane.getComponents.takeRight(15)
+    val (newSnakeLength, components, newRP) = if (Math.abs(randomPoint.xAxis - newX ) <= 10 && Math.abs(newY - randomPoint.yAxis) <= 10 && Math.abs(randomPoint.xAxis - newX ) >= 0 && Math.abs(newY - randomPoint.yAxis) >= 0) {
+      (snakeLength + 1 , frame.getContentPane.getComponents.reverse.tail.reverse, JSnakeBodyPart.generate(Color.RED))
+    }
+    else (snakeLength, frame.getContentPane.getComponents, randomPoint)
 
-    revalidate(frame, teleportation(newX), teleportation(newY),  components :+ new JSnakeBodyPart(newX, newY, Color.GREEN) , randomPoint)
-
+    revalidate(frame, newSnakeLength, teleportation(newX), teleportation(newY), components.takeRight(newSnakeLength) :+ new JSnakeBodyPart(newX, newY, Color.GREEN), newRP)
   }
 
   private def teleportation(current: Int, elite: Int = 600): Int = {
     if (current > elite) 0
-    else if(current < 0) elite else current
+    else if (current < 0) elite else current
   }
 
-  private def revalidate(frame: JFrame, headX: Int, headY: Int, snakeBody: Array[Component], randomPoint: JSnakeBodyPart): Unit = this.synchronized {
-    val listener = new WASDListener(reactToKey(_, x = headX, y = headY, frame = frame, randomPoint))
+  private def revalidate(frame: JFrame, snakeLength: Int, headX: Int, headY: Int, snakeBody: Array[Component], randomPoint: JSnakeBodyPart): Unit = {
+    val listener = new WASDListener(reactToKey(_, x = headX, y = headY, snakeLength, frame = frame, randomPoint))
     frame.getKeyListeners.foreach(frame.removeKeyListener)
     frame.addKeyListener(listener)
     frame.getContentPane.removeAll()
-
-    println(snakeBody.length)
 
     snakeBody.map { e =>
       frame.add(e)
     }
 
-
+    frame.revalidate()
+    frame.add(randomPoint)
     frame.revalidate()
   }
 }
--- Snake/src/main/scala/jelements/JSnakeBodyPart.scala
+++ Snake/src/main/scala/jelements/JSnakeBodyPart.scala
@@ -4,7 +4,7 @@ import java.awt.{Color, Graphics}
 
 import javax.swing.JComponent
 
-class JSnakeBodyPart(xAxis: Int, yAxis: Int, color: Color = Color.GREEN) extends JComponent {
+case class JSnakeBodyPart(xAxis: Int, yAxis: Int, color: Color = Color.GREEN) extends JComponent {
 
   override def paint(g: Graphics): Unit = {
     g.setColor(color)
@@ -18,23 +18,9 @@ object JSnakeBodyPart {
   private val r = new scala.util.Random
 
   def generate(color: Color): JSnakeBodyPart = {
-    val r1 = r.nextInt(600)
-    val r2 = r.nextInt(600)
+    val r1 = r.nextInt(400) + 100
+    val r2 = r.nextInt(400) + 100
 
     new JSnakeBodyPart(r1, r2, color)
-  }
-}
-
-case class Point(xAxis: Int, yAxis: Int, color: Color = Color.GREEN)
-
-class PointsContainer(seq: Seq[Point]) extends JComponent {
-
-  override def paint(g: Graphics): Unit = {
-    seq.map { v =>
-      g.setColor(v.color)
-      g.fillRect(v.xAxis, v.yAxis, 10, 10)
-      g.setColor(Color.BLACK)
-      g.drawRect(v.xAxis, v.yAxis, 10, 10)
-    }
   }
 }
