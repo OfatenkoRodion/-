6452b92a17fc718c3444e5578e7b1b23392be238
Ofatenko Rodion Olegovich <ext.rofatenko@tcsbank.ru>
2020-12-02 22:44:04 +0300

    ContraFunctorRealExample 4

--- MonadMagic/src/main/scala/primitives/ContraFunctorRealExample.scala
+++ MonadMagic/src/main/scala/primitives/ContraFunctorRealExample.scala
@@ -7,7 +7,15 @@ object ContraFunctorRealExample extends App {
   case class SelfEmployedPaymentOrder(paymentIds: Seq[Int])
 
   trait PaymentOrderSubmitter[A] {
+    self: PaymentOrderSubmitter[A] =>
+
     def submit(value: A): Boolean
+
+    def contramap[B](convert: B => A, func: B => Boolean) : PaymentOrderSubmitter[B] =
+      new PaymentOrderSubmitter[B] {
+        override def submit(value: B): Boolean =
+          self.submit(convert(value)) && func(value)
+      }
   }
 
   def submit[A](value: A)(implicit p: PaymentOrderSubmitter[A]): Boolean = p.submit(value)
@@ -22,14 +30,17 @@ object ContraFunctorRealExample extends App {
       override def submit(value: ExternalPaymentOrder): Boolean = value.paymentIds.length < 10
     }
 
-  implicit val SelfEmployedPaymentOrderSubmitter =
+ /* implicit val SelfEmployedPaymentOrderSubmitter =
     new PaymentOrderSubmitter[SelfEmployedPaymentOrder] {
       override def submit(value: SelfEmployedPaymentOrder): Boolean = value.paymentIds.length > 10 && value.paymentIds.head != 1
-    }
+    }*/
+
+  implicit def SelfEmployedPaymentOrderSubmitter(implicit submitter: PaymentOrderSubmitter[InternalPaymentOrder]): PaymentOrderSubmitter[SelfEmployedPaymentOrder] =
+    submitter.contramap(v => InternalPaymentOrder(v.paymentIds), _.paymentIds != 1)
 
   val submitRes1 = submit(InternalPaymentOrder(1 to 10))
   val submitRes2 = submit(ExternalPaymentOrder(1 to 10))
-  val submitRes3 = submit(SelfEmployedPaymentOrder(1 to 11))
+  val submitRes3 = submit(SelfEmployedPaymentOrder(1 to 10))
 
   println(submitRes1)
   println(submitRes2)
