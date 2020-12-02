42cde5f75f206228680e714a5d1f295b5256cfcb
Ofatenko Rodion Olegovich <ext.rofatenko@tcsbank.ru>
2020-12-02 22:24:08 +0300

    ContraFunctorRealExample 3

--- MonadMagic/src/main/scala/primitives/ContraFunctorRealExample.scala
+++ MonadMagic/src/main/scala/primitives/ContraFunctorRealExample.scala
@@ -4,6 +4,7 @@ object ContraFunctorRealExample extends App {
 
   case class InternalPaymentOrder(paymentIds: Seq[Int])
   case class ExternalPaymentOrder(paymentIds: Seq[Int])
+  case class SelfEmployedPaymentOrder(paymentIds: Seq[Int])
 
   trait PaymentOrderSubmitter[A] {
     def submit(value: A): Boolean
@@ -21,10 +22,17 @@ object ContraFunctorRealExample extends App {
       override def submit(value: ExternalPaymentOrder): Boolean = value.paymentIds.length < 10
     }
 
+  implicit val SelfEmployedPaymentOrderSubmitter =
+    new PaymentOrderSubmitter[SelfEmployedPaymentOrder] {
+      override def submit(value: SelfEmployedPaymentOrder): Boolean = value.paymentIds.length > 10 && value.paymentIds.head != 1
+    }
+
   val submitRes1 = submit(InternalPaymentOrder(1 to 10))
   val submitRes2 = submit(ExternalPaymentOrder(1 to 10))
+  val submitRes3 = submit(SelfEmployedPaymentOrder(1 to 11))
 
   println(submitRes1)
   println(submitRes2)
+  println(submitRes3)
 
 }
